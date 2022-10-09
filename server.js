7f8146b7657d9ab2deb99f997b1752225ef841ad
Toréa Patissier <p.torea@icloud.com>
2022-10-09 17:46:37 +0200

    démmarer un serveur basique

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ server.listen(process.env.PORT || 3000);
 /**
  * run npx nodemon tu run nodemon
  * Changer la l4
+ * Check sur postman, si la MAJ à eu lieux sans avoir à redémarrer node alors nodemon est effectif
  * https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb/6466231-demarrez-votre-serveur-node
  */
