3cdc7ecc3b0d78248a965f681f1e20ffa0120095
Toréa Patissier <p.torea@icloud.com>
2022-10-09 19:04:39 +0200

    Créer une route GET
    Le CORS définit comment les serveurs et les navigateurs interagissent, en spécifiant quelles ressources peuvent être demandées de manière légitime – par défaut, les requêtes AJAX sont interdites.
    
    Pour permettre des requêtes cross-origin (et empêcher des erreurs CORS), des headers spécifiques de contrôle d'accès doivent être précisés pour tous vos objets de réponse.

--- app.js
+++ app.js
@@ -2,26 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log('Requête reçue!');
-    next(); // Permet de passer à la requête suivante, sinon "First express n'est pas exécuté "
-});
-
-app.use((req,res,next) => {
-    res.status(201)
+app.use((req, res, next) => { //Header IMPORTANT
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req,res,next) => { // We can run http://localhost:3000 and get this message with any method (GET,POST,PUT..)
-    res.json({
-        message: "First express"
-    })
-    next();
-});
-
-app.use((req,res,next)=>{
-    console.log('Réponse envoyée avec succès!');
-});
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app; 
 
