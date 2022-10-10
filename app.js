b77336d305b2fc931f4493a986f2f0aba3b41325
Toréa Patissier <p.torea@icloud.com>
2022-10-10 23:37:23 +0200

    Enregistrez et récupérez des données
    save()  – enregistre un Thing ;
    
    find()  – retourne tous les Things ;
    
    findOne()  – retourne un seul Thing basé sur la fonction de comparaison qu'on lui passe (souvent pour récupérer un Thing par son identifiant unique).
    
    La méthode  app.get()  permet de réagir uniquement aux requêtes de type GET.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Thing = require('./models/Things'); //ajout
 const app = express();
 
 mongoose.connect('mongodb+srv://toreapat:041219Tp@cluster0.4xxfeao.mongodb.net/?retryWrites=true&w=majority',
@@ -17,33 +18,26 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/stuff',(req,res,next)=>{
-    console.log(req.body);
-    res.status(201).json({
-        message: "Objet créé"
-    })
+app.post('/api/stuff',(req,res,next)=>{//ajout
+  delete req.body._id; //enlève le champs id de la requête
+  const thing = new Thing({
+    ...req.body // va recopier les champs dans le body de la requête
+  });
+  thing.save()
+  .then(()=> res.status(201).json({message: 'Objet enregistré!'}))
+  .catch(error => res.status( 400).json({error}));
 })
 
-app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+app.get('/api/stuff/:id',(req,res,next) => { //ajout find by id
+  Thing.findOne({ _id: req.params.id })
+  .then(thing => res.status(200).json((thing)))
+  .catch(error => res.status(404).json({error}));
+});
+
+app.get('/api/stuff', (req, res, next) => {//ajout find all
+  Thing.find()
+  .then(things => res.status(200).json(things))
+  .catch(error => res.status(400).json({error}));
   });
 
-module.exports = app; 
+module.exports = app;  
